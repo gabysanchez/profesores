d036c8ac45ac5b1d9a16458942b298f70bf800f2
Gabriel <gabysanchez@github.com>
2021-10-19 12:37:57 +0200

    Fusion de imagenes

--- data/profesores.js
+++ data/profesores.js
@@ -3,7 +3,7 @@ import Profesor from '../entities/Profesor.js';
 const profesores = [
   new Profesor('Sergio', 29, 'DAWES', 100),
   new Profesor('Pablo', 40, 'DAWEC', 90),
-  new Profesor('Jose', 29, 'DIW', 50),
-  new Profesor('Enrique', 29, 'DAW', 60),
+  new Profesor('Jose Luis', 35, 'DIW', 50),
+  new Profesor('Enrique', 55, 'DAW', 60),
 ];
 export { profesores };
